93d6651ad89e1c7953aba60ee16ad9a9021ce703
nalexander34 <nalexander34@gatech.edu>
2020-10-17 23:34:57 +0700

    Update users schema

--- backend/users.js
+++ backend/users.js
@@ -3,14 +3,15 @@ const mongoose = require("mongoose");
 const user = mongoose.Schema({
     username: {
         type: String,
-        unique: true,
+        index: true,
         required: true,
     },
     roomCode: {
         type: String,
-        unique: true,
+        index: true,
         required: true,
     },
 });
 
+user.index({ roomCode: 1, username: 1 }, { unique: true });
 module.exports = mongoose.model("User", user);
