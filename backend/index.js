5147ce0d9c1a969b3bdf29da7570f305cbef8cbb
achencompass360 <abel.chen@compass360tech.com>
2020-10-17 14:02:42 -0400

    added addUser and deleteUser endpoints

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const cors = require("cors");
+app.use(cors());
+
 require("dotenv/config");
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true});
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,35 @@
 const express = require("express");
-const user = require("../users");
+const User = require("../users");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-	user.find()
+	User.find()
 	  .then((data) => res.status(200).json(data))
 	  .catch((err) => res.status(500).json(err));
+});
+
+router.post("/addUser", (req, res) => {
+	const user = new User({
+		username: req.body.username,
+		roomCode: req.body.roomCode,
+	});
+
+	user
+		.save()
+		.then((data) => {
+			res.status(200).json(data);
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		})
+});
+
+router.delete("/:userId", (req, res) => {
+	User.remove({ _id: req.params.userId })
+		.then((data) => res.status(200).json(data))
+		.catch((err) => {
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
