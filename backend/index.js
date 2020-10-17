965dbc5e6aba3537a31e52e384e74d33504919a5
achencompass360 <abel.chen@compass360tech.com>
2020-10-17 12:28:56 -0400

    git ignore changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 require("dotenv/config");
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true});
 
-const user = require(./routes/userRoute);
+const user = require("./routes/userRoute");
 app.use("/users", user);
 
 app.listen(3000);
