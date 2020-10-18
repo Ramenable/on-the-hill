1aa91a8edee4bc88133b330fda504f6e9759a9b1
achencompass360 <abel.chen@compass360tech.com>
2020-10-17 21:05:03 -0400

    something

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true, useNewUr
 const user = require("./routes/userRoute");
 app.use("/users", user);
 
+
 //localhost:5000/
  const server = app.listen(5000, function(){
      console.log("Server running on port 5000");
