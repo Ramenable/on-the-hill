4522696ceb4989c7c2e70c44280bdac3368506f3
John Chen <john.chen@compass360tech.com>
2020-10-17 23:47:47 -0400

    home table

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,53 +1,162 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import React, { useState } from "react";
+
+// // New Material-table styles:
+// import { forwardRef } from 'react';
+// import AddBox from '@material-ui/icons/AddBox';
+// import ArrowDownward from '@material-ui/icons/ArrowDownward';
+// import Check from '@material-ui/icons/Check';
+// import ChevronLeft from '@material-ui/icons/ChevronLeft';
+// import ChevronRight from '@material-ui/icons/ChevronRight';
+// import Clear from '@material-ui/icons/Clear';
+// import DeleteOutline from '@material-ui/icons/DeleteOutline';
+// // import Edit from '@material-ui/icons/Edit';
+// import FilterList from '@material-ui/icons/FilterList';
+// import FirstPage from '@material-ui/icons/FirstPage';
+// import LastPage from '@material-ui/icons/LastPage';
+// import Remove from '@material-ui/icons/Remove';
+// import SaveAlt from '@material-ui/icons/SaveAlt';
+// import Search from '@material-ui/icons/Search';
+// import ViewColumn from '@material-ui/icons/ViewColumn';
+
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+// import Checkbox from '@material-ui/core/Checkbox';
+// import IconButton from '@material-ui/core/IconButton';
+// import Tooltip from '@material-ui/core/Tooltip';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Switch from '@material-ui/core/Switch';
+// import DeleteIcon from '@material-ui/icons/Delete';
+// import FilterListIcon from '@material-ui/icons/FilterList';
+
+// import GridItem from "../../components/Grid/GridItem.js";
+// import GridContainer from "../../components/Grid/GridContainer.js";
+// // import Table from "../../components/Table/Table.js";
+// import Tasks from "../../components/Tasks/Tasks.js";
+// import CustomTabs from "../../components/CustomTabs/CustomTabs.js";
+// import Danger from "../../components/Typography/Danger.js";
+// import Card from "../../components/Card/Card.js";
+// import CardHeader from "../../components/Card/CardHeader.js";
+// import CardIcon from "../../components/Card/CardIcon.js";
+// import CardBody from "../../components/Card/CardBody.js";
+// import CardFooter from "../../components/Card/CardFooter.js";
+
+// import MaterialTable from "material-table";
+
+// import styles from "../../assets/jss/material-dashboard-react/views/dashboardStyle.js";
+
+// const useStyles = makeStyles(styles);
+// const tableIcons = {
+//   Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+//   Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+//   Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+//   Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+//   DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+//   Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+//   Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+//   Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+//   FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+//   LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+//   NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+//   PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+//   ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+//   Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+//   SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+//   ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+//   ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+// };
+// var tableData = []
 
 const useStyles = makeStyles({
-    root: {
-      minWidth: 275,
+    table: {
+        minWidth: 650,
     },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 12,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-  });
+});
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 function Home() {
+
     const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
+
     return (
-        <Card className={classes.root} variant="outlined">
-        <CardContent>
-          <Typography className={classes.title} color="black" gutterBottom>
-            Word of the Day
-          </Typography>
-          <Typography variant="h5" component="h2">
-            be{bull}nev{bull}o{bull}lent
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            adjective
-          </Typography>
-          <Typography variant="body2" component="p">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+        // <div>
+        //     <p>this is the home page!</p>
+
+        // </div>
+        // <h1>Material-Table Demo</h1> 
+        // <div style={{ maxWidth: "100%" }}>
+
+        //   <MaterialTable 
+        //     columns={[
+        //       { 
+        //         title: "Name", 
+        //         field: "name"
+        //       },
+        //       { 
+        //         title: "Occupation",
+        //         field: "job" 
+        //       }, 
+        //       { 
+        //         title: "Age", 
+                // field: "age",
+        //         type: "numeric"
+        //       }
+        //     ]}
+        //     data={data}
+        //     title="Material-Table Demo" 
+        //   />
+
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Dessert (100g serving)</TableCell>
+                        <TableCell align="right">Calories</TableCell>
+                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.calories}</TableCell>
+                            <TableCell align="right">{row.fat}</TableCell>
+                            <TableCell align="right">{row.carbs}</TableCell>
+                            <TableCell align="right">{row.protein}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+
+        //    </div> 
     )
 }
+
 export default Home;
